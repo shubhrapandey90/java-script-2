059c77d4e57963e5f944245a839fa15a7852756d
shubhra <gungun@pandey50.com>
2026-07-17 12:48:41 +0530

    datatypes-summary

--- 01basic/datatypes-summary.js
+++ 01basic/datatypes-summary.js
@@ -34,3 +34,34 @@ const myFunction = function(){
 // console.log(typeof outsideTemp);
 // console.log(typeof scoreValue);
 // console.log(typeof myFunction);
+
+
+
+// -----------------------------NEW TOPIC MEMORY--------------------------------//
+
+//types:
+//  Stack (Primitive), Heap (Non-Primitive)
+// In stack copy of memory is given,In heap reference of original memory is given.
+
+let myYoutubename = "shubhrapandeydotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+ console.log(myYoutubename);
+ console.log(anothername);
+
+ let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+ }
+let userTwo = userOne 
+
+userTwo.email = "shubhra@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+//stack revision in detail//
