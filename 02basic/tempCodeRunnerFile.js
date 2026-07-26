1fb3cd7abbf3a299edd8e4d69f154dbd3b6fe3db
shubhra <gungun@pandey50.com>
2026-07-26 14:26:35 +0530

    code runner

---
+++ 02basic/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const JsUser =  {
+    name:"Shubhra",
+    "full name": "Shubhra pandey",
+    [mysym]: "mykey1",
+    
+    age: 20,
+    location: "lucknow",
+    email: "shubra@lucknow.com",
+    isLoggedin: false,
+    lastLogginDays: ["Monday", "Saturday"]
+}
