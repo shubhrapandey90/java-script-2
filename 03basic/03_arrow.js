a4163212c4f4d9d51b22cf44efb3f15d3271daa5
shubhra <gungun@pandey50.com>
2026-08-02 01:29:30 +0530

    arrowsjs

---
+++ 03basic/03_arrow.js
@@ -0,0 +1,59 @@
+const user = {
+    username: "shubhra",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} ,  welcome to website`);
+        console.log(this);
+        
+        
+    }
+    
+}
+
+ //user.welcomeMessage()
+ //user.username = "sam"
+ //user.welcomeMessage()
+
+//console.log(this);
+
+
+// function chai(){
+//     let username = shubhra
+//     console.log(this.username);
+// }
+
+// //chai()
+
+// const chai = function(){
+//      let username = "shubhra"
+//      console.log(this.username);
+
+// }
+const chai = () => {
+     let username = "shubhra"
+     console.log(this);
+
+}
+chai()
+
+// const addTwo = (num1, num2) => {    // explicit return
+//     return num1 + num2
+// }
+
+
+ //const addTwo = (num1, num2) =>  num1 + num2  implicit return  
+
+ //onst addTwo = (num1, num2) =>  (num1 + num2) 
+
+ const addTwo = (num1, num2) =>  ({username: "shubhra"}) 
+
+
+console.log(addTwo(3, 4));
+
+
+
+
+
+
+
