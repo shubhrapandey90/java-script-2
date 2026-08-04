4c95a2a07f7d6a738048a816f086d1b4b4cf0e4c
shubhra <gungun@pandey50.com>
2026-08-04 23:22:01 +0530

    onefile

---
+++ 04ControlFlow/one.js
@@ -0,0 +1,63 @@
+// if
+// const isUserLoggedIn = true
+// const temprature = 28 
+
+// if ( temprature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temprature is greater than 50");
+// }
+
+
+// console.log("execute");
+
+// <, >, <=, >=, ==, ===(checks both value and price), !==
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`Use power: ${power}`);
+// }
+
+// console.log(`Use power: ${power}`);
+
+
+//const balance = 1000
+
+//if (balance > 500) console.log("test"),console.log("test2");  comma seperated lines should be ignored
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+    
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+    
+}
+
+if (LoggedInFromGoogle || LoggedInFromEmail ) {
+    console.log("User Logged in");
+    
+}
+
+
+
+
+
