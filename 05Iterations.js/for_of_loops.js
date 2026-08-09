132fea9eb114537c5ef4feb4dcacb5be2b8340de
shubhra <gungun@pandey50.com>
2026-08-09 20:18:12 +0530

    for_of_loops

---
+++ 05Iterations.js/for_of_loops.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4]
+
+for (const val of arr) {
+    //console.log(val);
+    
+}
+
+const greetings = "Hello world!"
+
+for (const greet of greetings) {
+   // console.log(`Each char is ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United states of america")
+map.set('FR', "FRANCE")
+
+//console.log(map);
+ 
+for (const [key, value] of map) {
+    //console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'spiderman'
+}
+// 
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+//     // my object is not iterable for forof syntax
+// }
