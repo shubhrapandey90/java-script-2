9ce9dcf4735448bd156c4a1c83ea43879937118a
shubhra <gungun@pandey50.com>
2026-08-11 21:02:06 +0530

    eight.js

--- 05Iterations.js/eight.js
+++ 05Iterations.js/eight.js
@@ -1,5 +1,35 @@
-const yNums = [1, 2, 3, 4]
+const myNums = [1, 2, 3, 4]
 
-const myToatal = myNums.reduce(function(acc, currval){
+// const myTotal = myNums.reduce(function(acc, currval){
+//     console.log(`acc ${acc} and currval: ${currval}`);
     
-})
+//     return acc + currval
+
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemname: "js course",
+        price: 2999
+    },
+    {
+        itemname: "python course",
+        price: 999
+    },
+    {
+        itemname: "java course",
+        price: 5999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc,item) => acc + item.price, 0)
+
+
+
+console.log(priceToPay);
+
